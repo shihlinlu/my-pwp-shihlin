d638bc70f6377c823424ee4906810569aa005a1d
Shihlin Lu <shihlinlu@gmail.com>
2017-04-18 02:08:59 -0600

    Created directories and milestone-1 template

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>MILESTONE 1</title>
+	</head>
+	<body>
+		<main>
+			<h1>MILESTONE 1</h1>
+			<h2>Purpose:</h2>
+			<h2>Audience:</h2>
+			<h2>Goal:</h2>
+			<ul>
+				<li>text goes here</li>
+				<li>text goes here</li>
+				<li>text goes here</li>
+			</ul>
+			<br>
+			<h1>Persona</h1>
+			<h3>Name:</h3>
+			<h3>Age:</h3>
+			<h3>Profession:</h3>
+			<h3>Technology:</h3>
+			<h3>Attitudes and Behaviors:</h3>
+			<h3>Frustrations and Needs:</h3>
+			<h3>Goals:</h3>
+
+			<h1>Use Case:</h1>
+
+		</main>
+	</body>
+</html>
