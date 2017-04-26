6962bea8276e3ea4f92dbe14f3f1e39f6a2574be
Shihlin Lu <shihlinlu@gmail.com>
2017-04-26 16:52:36 -0600

    Completed purpose, audience, goal, persona, and use case for Milestone-1.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,27 +6,48 @@
 		<title>MILESTONE 1</title>
 	</head>
 	<body>
-		<main>
+			<header>
 			<h1>MILESTONE 1</h1>
-			<h2>Purpose:</h2>
-			<h2>Audience:</h2>
-			<h2>Goal:</h2>
+			</header>
+		<main>
+			<h2>Purpose, Audience & Goal</h2>
+			<p></p>
+			<h3>Purpose:</h3>
+			<p>This website project is intended to present my professional portfolio, which demonstrates my capabilities in full stack web and software development, as well as technical writing. Additionally, my preferential interests in web and software development include, but not limited to the following industries: clean technology, building energy efficiency, sustainability, and robotics.</p>
+			<h3>Audience:</h3>
+			<p>This website is primarily intended for a professional audience, including employers, clients,software developers, investors, and technology-based entrepreneurs.</p>
+			<h3>Goal:</h3>
 			<ul>
-				<li>text goes here</li>
-				<li>text goes here</li>
-				<li>text goes here</li>
+				<li>Initiate contact from potential employers to obtain full-time employment</li>
+				<li>Initiate contact from other professionals for contract and freelance projects</li>
+				<li>Showcase my professional work in a digital space and build my personal online brand</li>
 			</ul>
-			<br>
-			<h1>Persona</h1>
-			<h3>Name:</h3>
-			<h3>Age:</h3>
-			<h3>Profession:</h3>
-			<h3>Technology:</h3>
-			<h3>Attitudes and Behaviors:</h3>
-			<h3>Frustrations and Needs:</h3>
-			<h3>Goals:</h3>
 
-			<h1>Use Case:</h1>
+			<h2>Persona</h2>
+			<h3>Name: Thomas Chu</h3>
+			<h3>Age: 35</h3>
+			<h3>Profession:</h3>
+			<p>Chief Technology Officer at Verdigris, an artificial intelligence driven, integrated software and hardware platform for energy management in commercial buildings. Recently, Fast Company has named Verdigris as one of the world's 10 most innovative energy companies.</p>
+			<h3>Technology:</h3>
+			<p>Thomas loves seamless integration with clean design, thus he is a Mac power user. He is a proud owner of the following devices:</p>
+				<ul>
+					<li>15 inch MacBook Pro</li>
+					<li>27 inch iMac with 5K Retina Display with 2 additional monitors (when you're dealing with large energy data sets, you need multiple screens to visualize and understand all the awesome yet complicated code)</li>
+					<li>iPhone 7 Plus with unlimited 4G data</li>
+					<li>12.9 inch iPad Pro with unlimited 4G data</li>
+				</ul>
+			<h3>Attitudes and Behaviors:</h3>
+			<p>Thomas spends a lot of time thinking important thoughts, such as how to help facility managers understand energy consumption patterns within the building and how to continue building the world's best energy management platform, and all the while brewing up more ideas utilizing artificial intelligence to battle the disease called climate change. He thrives on efficiency and automating as many things in his life. He also loves working with his team at Verdigris, where things get done and an imaginary cloud of innovative ideas are always welcome.</p>
+			<h3>Frustrations and Needs:</h3>
+			<p>Thomas is looking for a talented junior developer who would be an addition to the Applications or Platform team. With tight deadlines and schedules, he has had difficulty finding developers that are not only interested in working with cutting edge technology, but have a keen interest in solving global environmental problems. Without a doubt, there are many talented software developers in the Bay Area, but he has expanded his search for developers outside of California who can adapt to a remote work environment. He is looking to hire someone by mid June 2017.</p>
+			<h3>Goals:</h3>
+			<p>Hire a web and/or software developer to join the Applications or Platform team at Verdigris.</p>
+			<h4>User Story:</h4>
+			<p>As a user, Thomas needs to find evidence of a potential developer's work in order to determine if he or she qualifies for 2 rounds of interviews.</p>
+
+			<h2>Use Case</h2>
+			<p>Shihlin has sent an email on May 1, 2017 at 8am to Thomas on May expressing her interest in joining Verdigris as a software developer. She sent her resume and cover letter with request for feedback. Thomas quickly reads her email, but due to other important company meetings, he moves the email to the "Read Later" folder.</p>
+			<p>It's 3pm and Thomas finally has a small break before completing other tasks for the day. He opens Shihlin's email again and views her cover letter and resume. He then proceeds to her website to view her professional portfolio. Impressed by her previous projects and experience, he replies back indicating interest for a phone interview scheduled for the next week. Considering that Shihlin does not live in San Francisco or California, he needs time to think about remote employees joining Verdigris, which currently has all their employees working on-site. Thomas speaks with other executives at the technology and the final consensus is to proceed with the phone interview that could potentially result to an in-person interview.</p>
 
 		</main>
 	</body>
