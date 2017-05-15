3ea2d1a51fa82be125fc1881eb05cc8209f2b7fd
Shihlin Lu <shihlinlu@gmail.com>
2017-05-15 01:51:05 -0600

    added desktop wireframe

--- epic/public_html/documentation/milestone-2.php
+++ epic/public_html/documentation/milestone-2.php
@@ -11,7 +11,7 @@
 		</header>
 		<main>
 			<h2>Overview</h2>
-			<p>My website will have 4 sections that will encapsulate who I am as a new fullstack developer. Contemporary, minimalistic, and succinct design will complement the content to reflect my attention to structure and detail. Professional social media links and a contact form will also be shown.</p>
+			<p>My website will have 4 sections that will encapsulate who I am as a new full stack software & web developer. Contemporary, minimalistic, and succinct design will complement the content to reflect my attention to structure and detail. Professional social media links and a contact form will also be shown.</p>
 			<h3>Section 1: About</h3>
 			<p>My name, logo, and a brief summary of my background and interests will be displayed here.</p>
 			<h3>Section 2: Portfolio</h3>
@@ -19,10 +19,12 @@
 			<h3>Section 3: Writing</h3>
 			<p>A link to my blog and other notable writing posts will be shown here. I've included this section because I enjoy writing about certain topics that are highly important to me such as vegan nutrition research, running, and sustainability.</p>
 			<h3>Section 4: Contact</h3>
-			<p>A contact form and social media links will be shown (LinkedIn and Github)</p>
+			<p>A contact form and social media links will be shown (LinkedIn and Github).</p>
 			<h2>Wireframes</h2>
 			<h3>Mobile</h3>
-			<img src="images/wireframes-mobile.png" alt="wireframes-mobile"/>
+			<img src="images/wireframes-mobile.png" alt="mobile wireframe"/>
+			<h3>Desktop</h3>
+			<img src="images/wireframe-desktop.png" alt="desktop wireframe"/>
 		</main>
 
 	</body>
