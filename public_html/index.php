35c614fa3723c50ed6be44e21ac62a8b589d1ee2
Shihlin Lu <shihlinlu@gmail.com>
2017-06-05 11:40:22 -0600

    added css for nav bar, but not scrolling to correct sections

--- public_html/index.php
+++ public_html/index.php
@@ -23,6 +23,9 @@
 		<!-- Latest compiled and minifed JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+		<!-- CSS Reset -->
+		<link rel="stylesheet" href="css/reset.css">
+
 		<!-- CSS -->
 		<link rel="stylesheet" href="css/style.css" />
 
