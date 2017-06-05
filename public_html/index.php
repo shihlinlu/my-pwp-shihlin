9c93a3acf86cc4a02ba99771e2974a6b15e9fb4e
Shihlin Lu <shihlinlu@gmail.com>
2017-06-05 11:33:29 -0600

    created separate reset.css, added navigation bar, added content sections

--- public_html/index.php
+++ public_html/index.php
@@ -35,62 +35,98 @@
 		<!-- Particleground -->
 		<script type='text/javascript' src='js/jquery.particleground.js'></script>
 		<script type='text/javascript' src='js/demo.js'></script>
+
+		<!-- Modernizr.js -->
+		<script type='text/javascript' src="js/modernizr.js"></script>
+
+		<!-- Resource jQuery -->
+		<script type='text/javascript' src="js/main.js"></script>
+
 		<title>Shihlin Lu</title>
 	</head>
 
 	<body>
 		<header>
-				<!-- Mobile -->
-
-
 				<!-- Navigation Bar -->
-
+			<nav id="cd-vertical-nav">
+				<ul>
+					<li>
+						<a href="#intro" data-number="1">
+							<span class="cd-dot"></span>
+							<span class="cd-label">Introduction</span>
+						</a>
+					</li>
+					<li>
+						<a href="#about" data-number="2">
+							<span class="cd-dot"></span>
+							<span class="cd-label">About</span>
+						</a>
+					</li>
+					<li>
+						<a href="#portfolio" data-number="3">
+							<span class="cd-dot"></span>
+							<span class="cd-label">Portfolio</span>
+						</a>
+					</li>
+					<li>
+						<a href="#writing" data-number="4">
+							<span class="cd-dot"></span>
+							<span class="cd-label">Writing</span>
+						</a>
+					</li>
+					<li>
+						<a href="#contact" data-number="1" class="is-selected">
+							<span class="cd-dot"></span>
+							<span class="cd-label">Contact</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<a class="cd-nav-trigger cd-img-replace">Open navigation<span></span></a>
 		</header>
 
 		<div id="particles">
 			<div id="intro">
 				<h1>Shihlin Lu</h1>
 				<p>Software + Web Development</p>
+				<a href="about" class="cd-scroll-down cd-img-replace">Scroll Down</a>
 			</div>
 		</div>
 
 
+		<!-- <section id="intro" class="cd-section">
+			<h1>Shihlin Lu</h1>
+			<a href="about" class="cd-scroll-down cd-img-replace">Scroll Down</a>
+		</section><!-- /.cd-section -->
 
 		<!-- About Section -->
-		<section>
-			<div class="row content">
-				<h1 class="about">About</h1>
-				<p>About</p>
-			</div>
-		</section>
-
-
+		<section id="about" class="cd-section">
+			<h1>About</h1>
+			<p>Shihlin is awesome.</p>
+		</section><!-- /.cd-section -->
 
 		<!-- Portfolio Section -->
-		<section>
-			<div class="row content">
-				<h1 class="portfolio">Portfolio</h1>
-				<p>About</p>
-			</div>
+		<section id="portfolio" class="cd-section">
+		<h1>About</h1>
+		<p>Shihlin is awesome.</p>
+		</section><!-- /.cd-section -->
+
+		<!-- About Section -->
+		<section id="portfolio" class="cd-section">
+			<h1>Portfolio</h1>
+			<p>Shihlin's brilliant work.</p>
 		</section>
 
+		<!-- Portfolio Section -->
+		<section id="writing" class="cd-section">
+			<h1>Writing</h1>
+			<p>Shihlin also writes about various topics.</p>
+		</section>
 
 		<!-- Writing Section -->
-		<section>
-			<div class="row content">
-				<h1 class="writing">Writing</h1>
-				<p>About</p>
-			</div>
-		</section>
-
-
-
-		<!-- Contact Section -->
-		<section>
-			<div class="row content">
-				<h1 class="contact">Contact</h1>
-				<p>About</p>
-			</div>
+		<section id="contact" class="cd-section">
+			<h1>Contact</h1>
+			<p>How to contact Shihlin.</p>
 		</section>
 
 
