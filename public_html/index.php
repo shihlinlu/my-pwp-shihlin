0fa2b3574d11ba6bbee9ecf0d22d5d5b16dfced9
Shihlin Lu <shihlinlu@gmail.com>
2017-06-05 04:41:28 -0600

    particleground finally working

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" class="no-js">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8" />
+		<title>Particleground demo</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- Latest compiled and minifed CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" crossorigin="anonymous">
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,77 +18,60 @@
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-		<!-- Google Font -->
-		<link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet" type='text/css'>
-
-		<!-- CSS -->
-		<link rel="stylesheet" href="/public_html/css/style.css" type="text/css"/>
+		<!-- Latest compiled and minifed JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 		<!-- jQuery v3.0 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
-		<!-- Latest compiled and minifed JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/style.css" />
 
-		<!-- Particleground.js -->
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet" type='text/css'>
+
+		<!-- Particleground -->
 		<script type='text/javascript' src='../jquery.particleground.js'></script>
 		<script type='text/javascript' src='js/demo.js'></script>
-
-		<title>Shihlin Lu</title>
-
 	</head>
-	<body class="sfooter">
-		<div class="sfooter-content">
-			<header>
-				<!-- Navigation Bar -->
-				<nav class="navbar navbar-default">
-					<div class="container">
-						<div class="row">
-							<!-- Brand and toggle get grouped for better mobile display -->
-							<div class="navbar-header page-scroll">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
 
-							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="collapse navbar-collapse" id="navbar-collapse">
-								<ul class="nav navbar-nav">
-									<li class="active"><a href="#">Home</a></li>
-									<li><a href="#">About</a></li>
-									<li><a href="#">Portfolio</a></li>
-									<li><a href="#">Writing</a></li>
-									<li><a href="#">Contact</a></li>
-								</ul>
-							</div><!-- /.navbar collapse -->
-						</div>
+	<body>
+		<!-- Navigation Bar -->
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="row">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
 					</div>
-				</nav>
-			</header>
-			<div id="particles">
-				<div id="intro">
-					<h1>Shihlin Lu</h1>
-					<p>Software + Web Development</p>
 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Writing</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div><!-- /.navbar collapse -->
 				</div>
 			</div>
+		</nav>
 
-			<!-- Introduction section -->
+		<div id="particles">
+			<div id="intro">
+				<h1>Shihlin Lu</h1>
+				<p>Software + Web Development</p>
 
-			<!-- About section -->
+			</div>
+		</div>
 
-			<!-- Portfolio section -->
-
-			<!-- Writing section -->
-
-			<!-- Contact section -->
-		</div><!-- /.sfooter-content -->
-
-
-		<!-- FOOTER -->
 
 	</body>
 </html>
